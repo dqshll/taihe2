08421a03bde3b2579f135d5025986bb994e35613
dingqun <qun.ding@edisonx.cn>
2018-07-25 13:22:02 +0800

    somthing done for taihe2

--- js/zoom.js
+++ js/zoom.js
@@ -396,7 +396,7 @@ Zoom.prototype.repaint = function(tmpZoom) {
 
     console.log ('scale=' + scale + ' dx/ox=' + delta_x + '/' + out_x + ' dy/oy=' + delta_y + '/' + out_y);
     this.doFinalize = false;
-    if (scale < 1.0 || scale > 4) {
+    if (scale < 1.0 || scale > 2) {
         return false;
     } else if (delta_x > 0 || delta_x < (out_x * -1) || delta_y > 0 || delta_y < (out_y * -1)) {
         return false;

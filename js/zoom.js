5a9db1bfa4c2ff662210a7ce377809341a28d216
dingqun <qun.ding@edisonx.cn>
2018-07-26 18:36:15 +0800

    somthing done for taihe2

--- js/zoom.js
+++ js/zoom.js
@@ -311,6 +311,7 @@ function Zoom(elem, config, wnd) {
     };
 
     var setSrcAndDest = function(touches){
+        console.log('setSrcAndDest')
         me.srcCoords = getCoords(touches);
         me.destCoords = me.srcCoords;
     };
@@ -335,13 +336,16 @@ function Zoom(elem, config, wnd) {
 
     var handleZoom = handleTouchEvent(function(touches) {
         var numOfFingers = touches.length;
+        console.log('handleTouchEvent numOfFg = ' + numOfFingers)
         if (numOfFingers != me.curTouch){
+            console.log('fg num changed')
             me.curTouch = numOfFingers;
             me.finalize();
             if (numOfFingers != 0) {
                 setSrcAndDest(touches);
             }
         } else {
+            console.log('fg equal')
             setDest(touches);
             me.previewZoom();
         }
@@ -375,6 +379,7 @@ Zoom.prototype.previewZoom = function() {
 };
 
 Zoom.prototype.setZoom = function(newZoom) {
+    console.log('setZoom');
     // this.resultantZoom = newZoom;
     this.repaint(newZoom);
 };
@@ -411,6 +416,7 @@ Zoom.prototype.repaint = function(tmpZoom) {
 };
 
 Zoom.prototype.reset = function() {
+    console.log('reset');
     if (this.wnd.requestAnimationFrame) {
         this.isAnimationRunning = true;
         var Z = this.activeZoom;

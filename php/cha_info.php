ca1889571d0be7e218cbc1815e8cff974fcf0c9f
dingqun <qun.ding@edisonx.cn>
2018-08-01 12:45:32 +0800

    somthing done for taihe2

--- php/cha_info.php
+++ php/cha_info.php
@@ -64,9 +64,11 @@ function onQueryHandler () {
             echo "start time = $start_time end time = $end_time";
 
             if (!empty($start_time) && $curTimeStamp < $start_time) {
+                echo ('start time ok continue');
                 continue;
             }
             if (!empty($end_time) && $curTimeStamp > $end_time) {
+                echo ('start time ok continue');
                 continue;
             }
         

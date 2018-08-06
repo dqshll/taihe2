9cc6396830e8d4a8469c8de689d024480b4b05d1
dingqun <qun.ding@edisonx.cn>
2018-08-06 19:17:55 +0800

    somthing done for taihe2

--- php/cha_info.php
+++ php/cha_info.php
@@ -562,38 +562,72 @@ function toDTS($value) {
 }
 
 function createQRCodes($sid, $t) {
-    // $data = input('post.');
-    global $QR_FOLDER, $BUZZ_URL;
-    $filename = "$QR_FOLDER/$sid-$t.png";
+    // // $data = input('post.');
+    // global $QR_FOLDER, $BUZZ_URL;
+    // $filename = "$QR_FOLDER/$sid-$t.png";
 
-    include './QRcode.php';
+    // include './QRcode.php';
 
-    $longUrlString = "http://www.91qzb.com/thinkphp/public/index.php/api/index/weixin?type=h5&t=$t&cid=$sid&url=$BUZZ_URL";
-    $errorCorrectionLevel = 'H';    //容错级别  
-    $matrixPointSize = 6;           //生成图片大小  
-    //调用类方法（此时二维码已经生成，只是还未集成logo）
-    echo '1 -> ' . $longUrlString;
+    // $longUrlString = "http://www.91qzb.com/thinkphp/public/index.php/api/index/weixin?type=h5&t=$t&cid=$sid&url=$BUZZ_URL";
+    // $errorCorrectionLevel = 'H';    //容错级别  
+    // $matrixPointSize = 6;           //生成图片大小  
+    // //调用类方法（此时二维码已经生成，只是还未集成logo）
+    // echo '1 -> ' . $longUrlString;
 
-    QRcode::png($longUrlString, false, $errorCorrectionLevel, $matrixPointSize, 2);  
-    echo '2';
-    return;
-    $icon = 'edisonx_logo.png';
-    $code = ob_get_clean();
-    $code = imagecreatefromstring($code);
-    $logo = imagecreatefrompng($icon);
-    $QR_width = imagesx($code);//二维码图片宽度
-    $QR_height = imagesy($code);//二维码图片高度
-    $logo_width = imagesx($logo);//logo图片宽度
-    $logo_height = imagesy($logo);//logo图片高度
-    $logo_qr_width = $QR_width / 4;
-    $scale = $logo_width/$logo_qr_width;
-    $logo_qr_height = $logo_height/$scale;
-    $from_width = ($QR_width - $logo_qr_width) / 2;
+    // QRcode::png($longUrlString, false, $errorCorrectionLevel, $matrixPointSize, 2);  
+    // echo '2';
+    // return;
+    // $icon = 'edisonx_logo.png';
+    // $code = ob_get_clean();
+    // $code = imagecreatefromstring($code);
+    // $logo = imagecreatefrompng($icon);
+    // $QR_width = imagesx($code);//二维码图片宽度
+    // $QR_height = imagesy($code);//二维码图片高度
+    // $logo_width = imagesx($logo);//logo图片宽度
+    // $logo_height = imagesy($logo);//logo图片高度
+    // $logo_qr_width = $QR_width / 4;
+    // $scale = $logo_width/$logo_qr_width;
+    // $logo_qr_height = $logo_height/$scale;
+    // $from_width = ($QR_width - $logo_qr_width) / 2;
+    // //重新组合图片并调整大小
+    // imagecopyresampled($code, $logo, $from_width, $from_width, 0, 0, $logo_qr_width, $logo_qr_height, $logo_width, $logo_height);
+    // // header ( "Content-type: image/png" );
+    // ImagePng($code, $filename);
+    // echo 'done';
+    // // return "thinkphp/public/uploads/ticket/{$data['code']}.png";
+    require_once 'QRcode.php';
+    $value = 'http://www.chenle.top';     //二维码内容  
+    $errorCorrectionLevel = 'H'; //容错级别  
+    $matrixPointSize = 6;   //生成图片大小  
+    //生成二维码图片
+    $filename = microtime().'.png';
+    QRcode::png($value,$filename , $errorCorrectionLevel, $matrixPointSize, 2);  
+    
+    $logo = 'edisonx_logo.png';  //准备好的logo图片   
+    $QR = $filename;   //已经生成的原始二维码图  
+    
+    if (file_exists($logo)) {   
+    $QR = imagecreatefromstring(file_get_contents($QR));     //目标图象连接资源。
+    $logo = imagecreatefromstring(file_get_contents($logo));    //源图象连接资源。
+    $QR_width = imagesx($QR);   //二维码图片宽度   
+    $QR_height = imagesy($QR);   //二维码图片高度   
+    $logo_width = imagesx($logo);  //logo图片宽度   
+    $logo_height = imagesy($logo);  //logo图片高度   
+    $logo_qr_width = $QR_width / 4;    //组合之后logo的宽度(占二维码的1/5)
+    $scale = $logo_width/$logo_qr_width;    //logo的宽度缩放比(本身宽度/组合后的宽度)
+    $logo_qr_height = $logo_height/$scale;  //组合之后logo的高度
+    $from_width = ($QR_width - $logo_qr_width) / 2;   //组合之后logo左上角所在坐标点
+    
     //重新组合图片并调整大小
-    imagecopyresampled($code, $logo, $from_width, $from_width, 0, 0, $logo_qr_width, $logo_qr_height, $logo_width, $logo_height);
-    // header ( "Content-type: image/png" );
-    ImagePng($code, $filename);
-    echo 'done';
-    // return "thinkphp/public/uploads/ticket/{$data['code']}.png";
+    /*
+    * imagecopyresampled() 将一幅图像(源图象)中的一块正方形区域拷贝到另一个图像中
+    */
+    imagecopyresampled($QR, $logo, $from_width, $from_width, 0, 0, $logo_qr_width,$logo_qr_height, $logo_width, $logo_height); 
+    }   
+    
+    //输出图片  
+    imagepng($QR, 'qrcode.png');  
+    imagedestroy($QR);
+    imagedestroy($logo);
 }
 ?>

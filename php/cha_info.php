81daf49c3e884bafddb9bdcab33a442c0b3c6029
dingqun <qun.ding@edisonx.cn>
2018-08-07 10:47:08 +0800

    somthing done for taihe2

--- php/cha_info.php
+++ php/cha_info.php
@@ -571,28 +571,15 @@ function createQRCodes($sid, $t) {
 
     $shorten = "http://91qzb.com/api.php?format=json&domain=g&url=$longUrlString";
 
-    //初始化
     $curl = curl_init();
-    //设置抓取的url
     curl_setopt($curl, CURLOPT_URL, $shorten);
-    //设置头文件的信息作为数据流输出
-    // curl_setopt($curl, CURLOPT_HEADER, 1);
-    //设置获取的信息以文件流的形式返回，而不是直接输出。
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    //执行命令
     $data = curl_exec($curl);
-   
-    //显示获得的数据
     $json = json_decode($data);
-    var_dump($json);
-
     $shortUrl = $json->url;
-    echo 'short url = ' . $shortUrl;
+    curl_close($curl);
 
-     //关闭URL请求
-     curl_close($curl);
-
-    // handleOneQR($shortUrl, $filename);
+    handleOneQR($shortUrl, $filename);
 }
 function handleOneQR($url, $filename) {
     require_once 'QRcode.php';

25f8d811b8763991eb92f150f389c23d97ffd4a0
dingqun <qun.ding@edisonx.cn>
2018-08-07 10:39:00 +0800

    somthing done for taihe2

--- php/cha_info.php
+++ php/cha_info.php
@@ -578,7 +578,7 @@ function createQRCodes($sid, $t) {
     //设置头文件的信息作为数据流输出
     curl_setopt($curl, CURLOPT_HEADER, 1);
     //设置获取的信息以文件流的形式返回，而不是直接输出。
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     //执行命令
     $data = curl_exec($curl);
     //关闭URL请求

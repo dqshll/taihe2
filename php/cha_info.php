56bbd84233f5f1e4aa5037071da806cc85c3c445
dingqun <qun.ding@edisonx.cn>
2018-08-13 20:27:27 +0800

    somthing done for taihe2

--- php/cha_info.php
+++ php/cha_info.php
@@ -610,8 +610,7 @@ function createQRCodeVideo($sid, $dur) {
         handleOneQRCodes($sid, $t, $i);
     }
     
-    $cmd = "fmpeg -r 2 -i $QR_FOLDER/qr/$sid/$sid-%03d.png -c:v libx264 -pix_fmt yuv420p -y $QR_FOLDER/video/$sid.mp4";
-
+    $cmd = "ffmpeg -r 2 -i $QR_FOLDER/qr/$sid/$sid-%03d.png -c:v libx264 -pix_fmt yuv420p -y $QR_FOLDER/video/$sid.mp4";
     // echo $cmd;
     exec($cmd);
     $result['error'] = 0;

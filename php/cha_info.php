89c82749ca98bc20f35abe24d8246105e9fe818d
dingqun <qun.ding@edisonx.cn>
2018-08-16 18:19:50 +0800

    somthing done for taihe2_dev

--- php/cha_info.php
+++ php/cha_info.php
@@ -520,8 +520,6 @@ function logStat () {
     } else {
         $sql = "UPDATE find_stat SET duration='$dur', end_time='$end_time', repay_dur='$repay_dur' WHERE id='$user_id' AND $start_time='$start_time'";
     }
-
-    var_dump($sql) ;
     
     $db_result = mysql_query($sql);
     if (!$db_result) {
